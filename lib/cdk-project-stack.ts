f48d67ba2d531f4d7262e0c1863977b189c9fbb8
Riz Josel Urian <rurian@apple.com>
2026-01-11 21:49:42 +0800

    edit s3

--- lib/cdk-project-stack.ts
+++ lib/cdk-project-stack.ts
@@ -8,12 +8,12 @@ export class CdkProjectStack extends cdk.Stack {
     super(scope, id, props);
 
     //L1 and L2 Construct of an S3 Bucket
-    const level1S3Bucket = new CfnBucket(this, 'MyFirstLevel1ConstructBucket'{
+    const level1S3Bucket = new CfnBucket(this, 'MyFirstLevel1ConstructBucket',{
       versioningConfiguration:{
         status: "Enabled"
       }
     });
-    const level2S3Bucket = new Bucket(this, 'MyFirstLevel2ConstructBucket'{
+    const level2S3Bucket = new Bucket(this, 'MyFirstLevel2ConstructBucket',{
       versioned: true
     });
     // The code that defines your stack goes here
